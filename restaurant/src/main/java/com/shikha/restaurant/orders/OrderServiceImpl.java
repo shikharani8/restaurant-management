e3a33d80e91d8b57c85eba1b7a6b9e1add853b3a
Shikha Rani <shikharani8@gmail.com>
2023-05-05 02:17:45 +0100

    ...

--- restaurant/src/main/java/com/shikha/restaurant/orders/OrderServiceImpl.java
+++ restaurant/src/main/java/com/shikha/restaurant/orders/OrderServiceImpl.java
@@ -1,11 +1,15 @@
 package com.shikha.restaurant.orders;
 
+import com.shikha.restaurant.items.Item;
 import com.shikha.restaurant.users.User;
+import com.shikha.restaurant.users.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Shikha
@@ -15,6 +19,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository repository;
+    private final UserService userService;
+    private double TAX_PERCENTAGE = 10;
     @Override
     public List<Order> findOrderByUser(User user) {
         return repository.findByCustomerOrChef(user, user);
@@ -22,6 +28,25 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order createOrder(Order order) {
-        return null;
+        order.setCreatedAt(LocalDateTime.now());
+        order.setCustomer(userService.findLoggedInUser());
+        order.setChef(userService.findByUsername("chef")
+                .orElseThrow(() -> new RuntimeException("User not found")));
+        order.setPrices(calculatePrices(order.getItems()));
+        order.setTax(calculateTax(order.getPrices()));
+        order.setTotal(order.getPrices() + order.getTax());
+        return repository.save(order);
+    }
+
+    private double calculateTax(double prices) {
+        return prices * TAX_PERCENTAGE/100;
+    }
+
+    private double calculatePrices(Set<Item> items) {
+        double prices = 0.0;
+        for(Item item : items) {
+            prices = prices + Double.valueOf(item.getPrice());
+        }
+        return prices;
     }
 }
